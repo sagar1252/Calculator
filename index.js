aa5cf7a99d04afc9d89d5dda865e95b0fc3a7e4a
sagar1252 <114641159+sagar1252@users.noreply.github.com>
2024-07-29 19:59:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+const digits = document.getElementsByClassName("digit");
+for (let i = 0; i < digits.length; i++) {
+    digits[i].addEventListener("click", operations);
+}
+var inputTextBox=document.getElementById("inputText").addEventListener("click",operations);
+
+
+var buttonOne=document.getElementById("btn-1").addEventListener("click",operations);
+
+function operations(){
+
+
+
+}
